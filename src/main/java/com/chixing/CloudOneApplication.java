d797d988b214a2582653d15343edc5937057c2ef
lzh <lzh2018302@163.com>
2021-01-10 22:23:39 +0800

    "第一次提交"

---
+++ src/main/java/com/chixing/CloudOneApplication.java
@@ -0,0 +1,13 @@
+package com.chixing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chixing/CloudOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chixing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
